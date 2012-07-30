fc5348d416940c602a3e0b1fd37408ad33cfa2b9
Joshua Murray <joshua.murray@gmail.com>
2012-07-30 09:05:32 -0500

    access_token warning

--- callback.php
+++ callback.php
@@ -4,6 +4,16 @@
 
   if($_GET['openid_spark_code'] != '') { // We've been authorized by the end user.
     if ($api->Grant($_GET['openid_spark_code']) == true) { // ...and have granted an access token by Spark API.
+
+      /*  
+        IMPORTANT:  Do not store your access/refresh token in this manner in your production application.
+        Your tokens should never be stored in a cookie or passed directly to a web browser (or any application
+        that is not the API).
+
+        We only cut this corner here for the sake of demonstrating the flow quickly and without further
+        dependencies.
+      */
+
       // Store the access and refresh tokens, and send the user back to see
       // their listings.
       $_SESSION['access_token']  = $api->oauth_access_token;
